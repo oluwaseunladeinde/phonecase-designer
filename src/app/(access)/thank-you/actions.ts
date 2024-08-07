121869c1a7151a361313ba53503fe8dc4ed11573
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-08-07 13:50:17 +0100

    feat: update payment status with clerk authentication

--- src/app/(access)/thank-you/actions.ts
+++ src/app/(access)/thank-you/actions.ts
@@ -1,13 +1,12 @@
 'use server'
 
 import { db } from '@/db'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { currentUser } from '@clerk/nextjs/server';
 
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
-    const { getUser } = getKindeServerSession()
-    const user = await getUser()
+    const user = await currentUser();
 
-    if (!user?.id || !user.email) {
+    if (!user?.id || !user?.emailAddresses[0].emailAddress) {
         throw new Error('You need to be logged in to view this page.')
     }
 
@@ -22,8 +21,6 @@ export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
     })
 
     if (!order) throw new Error('This order does not exist.')
-
-
 
     if (order.isPaid) {
         return order
